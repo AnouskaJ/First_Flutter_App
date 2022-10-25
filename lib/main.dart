13f224dba4a0002121a9d1ec9e245630e677d11e
Anouska Jhunjhunwala <82711261+AnouskaJ@users.noreply.github.com>
2022-10-26 00:37:54 +0530

    Changed icon based on #2

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add_reaction_rounded),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
